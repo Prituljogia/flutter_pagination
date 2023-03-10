cd79b393fd036af9f6f9cdbef699a0f1e6db6842
Prituljogia <122267858+Prituljogia@users.noreply.github.com>
2023-03-10 20:27:47 +0530

    pagination

--- example/lib/homepage.dart
+++ example/lib/homepage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pagination/flutter_pagination.dart';
 import 'package:http/http.dart' as http;
-import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 import 'model/passenger_data_model.dart';
 
@@ -12,41 +12,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int currentpage = 1;
 
+static const _currentpage=1;
   late int totalpages;
 
   List<Datum> passengers = [];
-  final RefreshController refreshController =
-      RefreshController(initialRefresh: true);
+  final PagingController<int, Datum> _pagingController =
+      PagingController(firstPageKey: 0);
 
-  Future<bool> getPassangersData({bool isRefresh = false}) async {
-    if (isRefresh) {
-      currentpage = 1;
-    } else {
-      if (currentpage >= totalpages) {
-        refreshController.loadNoData();
-        return false;
-      }
-    }
-
+  Future<bool> getPassangersData(int pageKey) async {
     final Uri uri = Uri.parse(
-        "https://api.instantwebtools.net/v1/passenger?page=$currentpage&size=10");
+        "https://api.instantwebtools.net/v1/passenger?page=$_currentpage&size=10");
     final response = await http.get(uri);
 
     if (response.statusCode == 200) {
       final result = passengersDataFromJson(response.body);
-
-      if (isRefresh) {
-        passengers = result.data;
-      } else {
-        passengers.addAll(result.data);
-      }
-
-
-      currentpage++;
       totalpages = result.totalPages;
-
       print(response.body);
       setState(() {});
       return true;
@@ -56,49 +37,47 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void initState() {
+    _pagingController.addPageRequestListener((pageKey) {
+      getPassangersData(pageKey);
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: Text("PassangerData Api calling",
-              style: TextStyle(color: Colors.white)),
-        ),
-        body: SmartRefresher(
-          controller: refreshController,
-          enablePullUp: true,
-          onRefresh: () async {
-            final result = await getPassangersData(isRefresh: true);
-            if (result) {
-              refreshController.refreshCompleted();
-            } else {
-              refreshController.refreshFailed();
-            }
-          },
-          onLoading: () async {
-            final result = await getPassangersData();
-            if (result) {
-              refreshController.loadComplete();
-            } else {
-              refreshController.loadFailed();
-            }
-          },
-          child: ListView.separated(
-              itemBuilder: (context, index) {
-                final passenger = passengers[index];
+      body:Container(
 
-                return ListTile(
-                  leading: CircleAvatar(
-                      radius: 25,
-                      child: Image.network(
-                        passenger.airline.first.logo,
-                        fit: BoxFit.cover,
-                      )),
-                  title: Text(passenger.trips.toString()),
-                  subtitle: Text(passenger.airline.first.country),
-                );
-              },
-              separatorBuilder: (context, index) => Divider(),
-              itemCount: passengers.length),
-        ));
+          child: RefreshIndicator(
+            onRefresh: () => Future.sync(
+                  () => _pagingController.refresh(),
+            ),
+            child: Container(
+              child: PagedListView<int, Datum>.separated(
+                pagingController: _pagingController,
+                builderDelegate: PagedChildBuilderDelegate<Datum>(
+                    animateTransitions: true,
+                    itemBuilder: (context, int, index) {
+                      final passenger = passengers[index];
+
+                      return ListTile(
+                        leading: CircleAvatar(
+                            radius: 25,
+                            child: Image.network(
+                              passenger.airline.first.logo,
+                              fit: BoxFit.cover,
+                            )),
+                        title: Text(passenger.trips.toString()),
+                        subtitle: Text(passenger.airline.first.country),
+                      );
+                    }),
+                separatorBuilder: (context, index) => const Divider(),
+              ),
+            ),
+          )
+      )
+       );
+
   }
 }
